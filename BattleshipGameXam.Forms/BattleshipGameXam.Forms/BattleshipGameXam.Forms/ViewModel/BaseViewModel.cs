bf3e10459140dd30d56cdff5a4b34939b143778f
Marineata Emil <marineataemil@gmail.com>
2021-11-02 00:41:24 +0200

    Added BaseViewModel and deleted pre added file.

---
+++ BattleshipGameXam.Forms/BattleshipGameXam.Forms/BattleshipGameXam.Forms/ViewModel/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BattleshipGameXam.Forms.ViewModel
+{
+    internal class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if(propertyName == null || PropertyChanged == null)
+            {
+                return;
+            }
+
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
